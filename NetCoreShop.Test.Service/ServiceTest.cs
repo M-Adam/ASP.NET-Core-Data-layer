3f3271a3e017fa058dc5a6046db64aeb728c673d
Adam Mołczan <adam.molczan@britenet.com.pl>
2016-07-31 18:24:12 +0200

    Testing update

---
+++ NetCoreShop.Test.Service/ServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Threading.Tasks;
+using Moq;
+using NetCoreShop.Data.Repositories;
+using NetCoreShop.Service;
+using Xunit;
+
+namespace NetCoreShop.Test.Service
+{
+    public class ServiceTesting
+    {
+        [Fact]
+        public void ServiceTestWorks()
+        {
+            var repo = new Mock<IRepositoryTest>();
+
+            //a
+            var service = new NetCoreShop.Service.ServiceTest(repo.Object);
+            
+            //a
+            var str = service.Test();
+
+            //a
+            Assert.Equal("Test", str);
+        }
+    }
+}
---
+++ NetCoreShop.Test.Web/Common/FirstTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace NetCoreShop.Test.Web.Common
+{
+    public class FirstTest
+    {
+        [Fact]
+        public void TwoPlusTwoEqualsFour()
+        {
+            var result = 2 + 2;
+
+            Assert.Equal(4, result);
+        }
+        
+    }
+}
